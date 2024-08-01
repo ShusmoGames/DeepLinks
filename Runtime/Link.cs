020d5fae0eb58ed0550d1b6be7ee962402a57657
athabsem <alaa@shusmo.io>
2024-08-01 12:59:50 +0300

    Initial

---
+++ Runtime/Link.cs
@@ -0,0 +1,57 @@
+using System;
+using UnityEngine;
+
+namespace Shusmo
+{
+    public partial class ShusmoAPI
+    {
+        public static class Link
+        {
+            public static Action<string> OnActivation;
+            private static string id = string.Empty;
+
+            /// <summary>
+            /// The tool ID provided on Initialize.
+            /// </summary>
+            public static string ID => id;
+
+            /// <summary>
+            /// Initialize the tool.
+            /// </summary>
+            /// <param name="gameName">The ID will be used on the tool</param>
+            public static void Init(string gameName)
+            {
+                id = gameName;
+                Application.deepLinkActivated += HandleDeepLinkActivation;
+                OnActivation?.Invoke("DeeoLink_Init");
+            }
+
+            private static void HandleDeepLinkActivation(string url)
+            {
+                // Check if the game was launched from a deep link
+                if (!string.IsNullOrEmpty(url))
+                {
+                    try
+                    {
+                        string message = url.Split("message=")[1];
+                        OnActivation?.Invoke(message);
+                    }
+                    catch { }
+                }
+            }
+
+            /// <summary>
+            /// Generate a URL text from a message.
+            /// </summary>
+            /// <param name="message">The desired message</param>
+            /// <returns>URL with the message</returns>
+            public static String GenerateMessage(string message) => $"https://shusmo.io/{id}?message={message}";
+
+            /// <summary>
+            /// Activate the tool with a URL.
+            /// </summary>
+            /// <param name="url">URL for read the message from it</param>
+            public static void Activate(string url) => HandleDeepLinkActivation(url);
+        }
+    }
+}
---
+++ Samples~/DemoSample1/DeepLinkHandler.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using TMPro;
+using Shusmo;
+
+public class DeepLinkHandler : MonoBehaviour
+{
+    [SerializeField] private TMP_Text messageText;
+    [SerializeField] private TMP_InputField inputField;
+
+    void Start() => ShusmoAPI.Link.Init("gamename");
+
+    void OnEnable() => ShusmoAPI.Link.OnActivation += HandleDeepLinkActivation;
+
+    void OnDisable() => ShusmoAPI.Link.OnActivation -= HandleDeepLinkActivation;
+
+    private void HandleDeepLinkActivation(string message) 
+    {
+        // Check if the game was launched from a deep link
+        if (!string.IsNullOrEmpty(message))
+            messageText.text = message;
+    }
+
+    public void Open() => Application.OpenURL(ShusmoAPI.Link.GenerateMessage(inputField.text));
+}
